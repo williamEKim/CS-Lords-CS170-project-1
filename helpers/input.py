a25fe3f2c54719e6db1151c048400c5881d1f2ac
Andrew Pham <andrewpham@ucr-secure-45-10-13-230-140.wnet.ucr.edu>
2025-10-30 18:55:09 -0700

    Organized files and created Makefile

--- helpers/input.py
+++ helpers/input.py
@@ -1,6 +1,6 @@
 from typing import List
-from misplacedH import getMisplacedHuristic
-from euclideanH import getEuclideanHuristic
+from helpers.misplacedH import getMisplacedHuristic
+from helpers.euclideanH import getEuclideanHuristic
 
 
 class PuzzleInput:
@@ -114,13 +114,3 @@ class PuzzleInput:
     def get_goal(self) -> List[List[int]]:
         """Return the 2D goal_state list."""
         return self.goal_state
-
-
-# in case this is imported in other python file
-if __name__ == "__main__":
-    builder = PuzzleInput()
-    builder.take_input()
-    print("Misplaced huristics " + str(getMisplacedHuristic(builder.get_puzzle(), builder.get_goal(), builder.get_n())))
-    euclidean_dist = getEuclideanHuristic(builder.get_puzzle(), builder.get_goal(), builder.get_n())
-    euclidean_dist = round(euclidean_dist, 3)
-    print("Euclidean Huristic(rounded): " + str(euclidean_dist))
