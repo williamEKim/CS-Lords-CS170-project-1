f9f3a5b3309a117bbba08afc8e422aefe08966c9
williamEkim <dongyeon0308@gmail.com>
2025-11-02 22:39:26 -0800

    updated main.py for better display and logical parts

--- main.py
+++ main.py
@@ -28,27 +28,43 @@ while True:
     except ValueError:
         print(f"It is not an appropriate value. \nPlease choose between option [1, 2, 3]\n")
 
-
+search: str = ""
 if search_choice == 1:
     result = uniform_cost_search(start_state, goal)
+    search = "Uniform Cost Search"
 elif search_choice == 2:
     result = a_star_search(start_state, goal, "misplaced")
+    search = "A Star Search with Misplaced Tile Huristic"
 else:
     result = a_star_search(start_state, goal, "euclidean")
+    search = "A Star Search with Euclidean Huristic"
 
 
 try:
     if result["solved"]:
-        print("\n=== Uniform Cost Search Solution ===")
+        if search_choice == 1:  # UCS
+            for i, grid in enumerate(result["path"]):
+                print(f"\nStep {i}, with g(n) = {i}:")
+                print(grid_print(grid))
+
+            print(f"\n=== {search} Solution ===")
+            # print(f"Cost (moves): {result['cost']}")
+
+        else:  # A* (misplaced or Euclidean)
+            g_val = 0
+            h_val = 0
+            for i, (grid, h_value) in enumerate(zip(result["path"], result["h_values"])):
+                print(f"\nStep {i}, with g(n) = {i} and h(n) = {h_value}:")
+                print(grid_print(grid))
+
+            print(f"\n=== {search} Solution ===")
+
         print(f"Cost (moves): {result['cost']}")
         print(f"Goal depth:   {result['goal_depth']}")
         print(f"Nodes expanded: {result['nodes_expanded']}")
         print(f"Max queue size: {result['max_queue_size']}")
         print("\nMoves:", " -> ".join(result["moves"]) or "(none)")
-        print("\nPath:")
-        for i, grid in enumerate(result["path"]):
-            print(f"\nStep {i}:")
-            print(grid_print(grid))
+
     else:
         print("No solution found.")
 
