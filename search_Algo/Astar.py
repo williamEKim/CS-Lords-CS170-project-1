28642e6c74cd94bc063b179423aa1092e2dd98e5
williamEkim <dongyeon0308@gmail.com>
2025-11-02 22:25:13 -0800

    updated the code so it returns the search result with g() h() on each node

--- search_Algo/Astar.py
+++ search_Algo/Astar.py
@@ -1,5 +1,7 @@
 from typing import List, Tuple, Dict, Optional
 import heapq
+from helpers.misplacedH import getMisplacedHuristic
+from helpers.euclideanH import getEuclideanHuristic 
 
 Grid = List[List[int]]
 State = Tuple[int, ...]  #flattens grid
@@ -41,5 +43,94 @@ def neighbors(state: State, n: int) -> List[tuple[str, State]]:
         res.append(("Right", swap(idx, idx + 1)))
     return res
 
-def A_star_search(start_state_grid: Grid, goal_grid: Grid) -> Dict[str, object]:
-    return ["", []]
+
+def a_star_search(start_state_grid: Grid, goal_grid: Grid, heuristic="euclidean") -> Dict[str, object]:
+    n = len(start_state_grid)
+    start_state = flatten(start_state_grid)
+    goal_state = flatten(goal_grid)
+
+    # depending on choice of huristic, set the huristic algorithm (by default euclidean)
+    def h(state: State) -> float:
+        grid = unflatten(state, n)
+        if heuristic == "euclidean":
+            return getEuclideanHuristic(grid, goal_grid, n)
+        else:
+            return getMisplacedHuristic(grid, goal_grid, n)
+
+    prio_queue = []
+    heapq.heappush(prio_queue, (h(start_state), 0, start_state))
+
+    g_cost: Dict[State, int] = {start_state: 0}
+    h_cost: Dict[State, float] = {start_state: h(start_state)}
+
+    # parent = {start_state: (None, None)}
+    parent: Dict[State, Tuple[Optional[State], Optional[str]]] = {start_state: (None, None)}
+
+    nodes_expanded = 0
+    max_queue_size = 1
+
+
+    while prio_queue:
+        max_queue_size = max(max_queue_size, len(prio_queue))
+        _, g, s = heapq.heappop(prio_queue) # not using f
+        if s == goal_state:
+            #reconstruct path
+            moves: List[str] = []
+            states: List[State] = []
+            costs: List[Tuple[int, float]] = []  # list of (g, h)
+
+            cur = s
+            while cur is not None:
+                states.append(cur)
+                # Append g and h for this state
+                costs.append({
+                    "g_value": g_cost[cur],
+                    "h_value": h_cost.get(cur, 0.0)
+                })
+
+                p, mv = parent[cur]
+                if mv is not None:
+                    moves.append(mv)
+                cur = p
+            
+            # reverse to match the order of path
+            states.reverse()
+            moves.reverse()
+            costs.reverse()
+            path_grids = [unflatten(st, n) for st in states]
+
+            return {
+                "path": path_grids,
+                "moves": moves,
+                "cost": g,
+                "costs": costs,
+                "nodes_expanded": nodes_expanded,
+                "max_queue_size": max_queue_size,
+                "goal_depth": g,
+                "solved": True,
+            }
+
+        #keep track of # of nodes expanded
+        nodes_expanded += 1
+        for move, next_state in neighbors(s, n):
+            new_g = g + 1
+            new_h = h(next_state) # now, we are using new huristic for A star instead of counter
+            if new_g < g_cost.get(next_state, float("inf")):
+                g_cost[next_state] = new_g
+                h_cost[next_state] = new_h
+                parent[next_state] = (s, move)
+                new_f = new_g + new_h 
+                heapq.heappush(prio_queue, (new_f, new_g, next_state))
+
+
+    # if there is no solution
+    return {
+            "path": [],
+            "moves": [],
+            "costs": [],
+            "cost": 0,
+            "nodes_expanded": nodes_expanded,
+            "max_queue_size": max_queue_size,
+            "goal_depth": g,
+            "solved": False,
+    }
